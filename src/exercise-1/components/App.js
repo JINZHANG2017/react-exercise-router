fda80aff7d51cf369fd449d038c6b27f58974e99
JINZHANG2017 <zj574406254@hotmail.com>
2020-10-12 15:09:54 +0800

    [ZhangJin] feat: finish exer

--- src/exercise-1/components/App.js
+++ src/exercise-1/components/App.js
@@ -1,13 +1,30 @@
 import React, {Component} from 'react';
 import '../styles/App.css';
-import {BrowserRouter as Router} from 'react-router-dom';
-
+import { BrowserRouter, NavLink, Route } from 'react-router-dom';
+import Home from './Home';
+import Profile from './Profile';
+import About from './About';
+// import "jquery/dist/js/jquery.js"
+// import "bootstrap/dist/css/bootstrap.css";
+// import 'bootstrap/dist/js/bootstrap';
 class App extends Component {
   render() {
     return (
-      <div className="app">
-        <Router />
-      </div>
+      <BrowserRouter>
+        <h1>TOP</h1>
+        <NavLink className="link" exact to="/" activeStyle={{
+                textDecoration: "underline",
+              }}>Home</NavLink>
+        <NavLink className="link" exact to="/my-profile" activeStyle={{
+                textDecoration: "underline",
+              }}>My Profile</NavLink>
+        <NavLink className="link" exact to="/about-us" activeStyle={{
+                textDecoration: "underline",
+              }}>About Us</NavLink>
+        <Route exact path="/" component={Home}/>
+        <Route exact path="/my-profile" component={Profile}/>
+        <Route exact path="/about-us" component={About}/>
+      </BrowserRouter>
     );
   }
 }
---
+++ src/exercise-1/components/Profile.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Profile extends Component {
+  render() {
+    return <h1>This is my-profile page</h1>;
+  }
+}
+
+export default Profile;
